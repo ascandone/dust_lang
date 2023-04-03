0901b0541a02ddfd489f32643956346eefa92928
Alessandro Scandone <alescandone@gmail.com>
2023-04-04 01:59:24 +0200

    refactored

--- cli/src/dust_json.rs
+++ cli/src/dust_json.rs
@@ -1,9 +1,24 @@
 use serde::{Deserialize, Serialize};
+use std::fs;
 
-pub const DUST_JSON_NAME: &'static str = "dust.json";
+const DUST_JSON_NAME: &'static str = "dust.json";
 
 #[derive(Serialize, Deserialize)]
 pub struct DustJson {
     #[serde(rename = "source-directories")]
     pub source_directories: Vec<String>,
+}
+
+impl DustJson {
+    pub fn read() -> Option<DustJson> {
+        let content = fs::read_to_string(DUST_JSON_NAME).ok()?;
+        let dust_json = serde_json::from_str::<DustJson>(&content).ok()?;
+        Some(dust_json)
+    }
+
+    pub fn write(&self) -> Result<(), ()> {
+        let serialized = serde_json::to_string_pretty(self).unwrap();
+        let () = fs::write(DUST_JSON_NAME, serialized).map_err(|_| ())?;
+        Ok(())
+    }
 }
--- cli/src/subcommands/init.rs
+++ cli/src/subcommands/init.rs
@@ -1,4 +1,4 @@
-use crate::dust_json::{DustJson, DUST_JSON_NAME};
+use crate::dust_json::DustJson;
 use argh::FromArgs;
 use std::fs;
 
@@ -11,16 +11,19 @@ const SRC_FOLDER: &'static str = "src";
 
 impl Init {
     pub fn run(&self) {
-        // TODO check if d.json already exists
+        let current = DustJson::read();
+        if current.is_some() {
+            println!("dust.json already exists");
+            return;
+        }
+
         let _ = fs::create_dir(SRC_FOLDER);
 
         let dust_json = DustJson {
             source_directories: vec![SRC_FOLDER.to_string()],
         };
 
-        let serialized = serde_json::to_string_pretty(&dust_json).unwrap();
-
-        let () = fs::write(DUST_JSON_NAME, serialized).unwrap();
+        dust_json.write().unwrap();
 
         println!("Done")
     }

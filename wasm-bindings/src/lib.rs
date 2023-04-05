ad7b333638094d06a938398e27a7e8a418b7c2fa
ascandone <alescandone@gmail.com>
2023-04-05 16:26:27 +0200

    wasm bindings POC

---
+++ wasm-bindings/src/lib.rs
@@ -0,0 +1,25 @@
+use core::interpreter;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+pub struct ReplOutput {
+    result: Result<Vec<String>, String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct EvalOutput {
+    result: Result<String, String>,
+}
+
+#[wasm_bindgen]
+pub fn eval_src(src: &str) -> JsValue {
+    let result = match interpreter::eval(src) {
+        Ok(value) => Ok(value.to_string()),
+        Err(e) => Err(format!("{:?}", e)),
+    };
+
+    let ev = EvalOutput { result };
+
+    serde_wasm_bindgen::to_value(&ev).unwrap()
+}

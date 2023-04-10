b030624723ec751d6a4d6cb41f3e5b0cf188e3ae
Alessandro Scandone <alescandone@gmail.com>
2023-04-10 10:25:01 +0200

    renamed

--- core/src/pretty.rs
+++ core/src/pretty.rs
@@ -79,7 +79,7 @@ impl PPrint {
 impl Display for PPrint {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let doc = Doc::Group(Box::new(self.doc.clone()));
-        let mut k = 0;
+        let mut width = 0;
         let mut vec = VecDeque::from([(0 as isize, Mode::Flat, &doc)]);
 
         loop {
@@ -105,12 +105,12 @@ impl Display for PPrint {
                 }
                 Doc::Text(s) => {
                     write!(f, "{s}")?;
-                    k += s.len() as isize;
+                    width += s.len() as isize;
                 }
                 Doc::Break(s) => match m {
                     Mode::Flat => {
                         write!(f, "{s}")?;
-                        k += s.len() as isize;
+                        width += s.len() as isize;
                     }
                     Mode::Break => {
                         let indent = str::repeat(" ", i as usize);
@@ -122,7 +122,7 @@ impl Display for PPrint {
                     // TODO vec.clone() is O(n)
                     let mut cloned_vec = vec.clone();
                     cloned_vec.push_front((i, Mode::Flat, x));
-                    let fits = self.fits(self.max_w - k, cloned_vec);
+                    let fits = self.fits(self.max_w - width, cloned_vec);
                     let mode = if fits { Mode::Flat } else { Mode::Break };
                     vec.push_front((i, mode, x));
                 }

94f1970ea594fa7b665368713311018f8bd5e113
Alessandro Scandone <alescandone@gmail.com>
2023-04-11 09:47:26 +0200

    added formatter wasm bindings

--- wasm-bindings/src/lib.rs
+++ wasm-bindings/src/lib.rs
@@ -1,4 +1,6 @@
+use core::formatter;
 use core::interpreter;
+use core::parser;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
@@ -20,6 +22,23 @@ pub fn eval_src(src: &str) -> JsValue {
     };
 
     let ev = EvalOutput { result };
+
+    serde_wasm_bindgen::to_value(&ev).unwrap()
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FormatOutput {
+    result: Result<String, String>,
+}
+
+#[wasm_bindgen()]
+pub fn format(src: &str) -> JsValue {
+    let result = match parser::parse(src) {
+        Ok(program) => Ok(formatter::format(program)),
+        Err(e) => Err(format!("{:?}", e)),
+    };
+
+    let ev = FormatOutput { result };
 
     serde_wasm_bindgen::to_value(&ev).unwrap()
 }
